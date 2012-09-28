7f90b6986dc7d9dd8ca3e6615109d79814193380
Stephen Handley <stephen.handley@gmail.com>
2012-09-27 23:50:24 -0700

    rename before/after options

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var Path = require('path');
 var Asserts = require('asserts');
 
 Asserts.dir({
-  before: function (filepath) {
+  before_each: function (filepath) {
     var configwd_path = Path.normalize(Path.join(__dirname, '..', 'index.js'));
     delete require.cache[configwd_path];
     process.chdir(filepath);
